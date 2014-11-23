a616ae4841db054ba6ad7e429f5fcd104cb1b14e
jpatrickbennett <patrick@elderfields.com>
2014-11-23 18:30:54 -0500

    updating description

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,12 @@
 run_analysis <- function(){
         #This script takes the Samsung data and processes it by doing the following:
-        # 1. Merges the training and the testing datasets
-        # 2. 
+        # 1. Merges the training and the test sets to create one data set.
+        # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+        # 3. Uses descriptive activity names to name the activities in the data set
+        # 4. Appropriately labels the data set with descriptive variable names. 
+        # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+        #
+        # The output is the summary data from step 5
         
         
         #reads in the X data for the test and train X data, and combines it into one
