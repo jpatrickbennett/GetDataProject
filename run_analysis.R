7a1fb0612b980faa0db6bd06f59edbb503e4b8c2
jpatrickbennett <patrick@elderfields.com>
2014-11-23 18:27:47 -0500

    Class Project for Getting and Cleaning Data
    This is the class project that processes Samsung wearable data into a
    tidy dataset and performs a filtering/summarizing of the data.

---
+++ run_analysis.R
@@ -0,0 +1,53 @@
+run_analysis <- function(){
+        #This script takes the Samsung data and processes it by doing the following:
+        # 1. Merges the training and the testing datasets
+        # 2. 
+        
+        
+        #reads in the X data for the test and train X data, and combines it into one
+        testX <- read.table("./UCI HAR Dataset/test/X_test.txt")
+        trainX <- read.table("./UCI HAR Dataset/train/X_train.txt")
+        combinedX <- rbind(testX, trainX)
+        
+        #reads in the descriptive variable names and applies it to the data
+        varLabels <- read.table("./UCI HAR Dataset/features.txt")
+        colnames(combinedX) <- varLabels[,2]
+        
+        
+        #reads in the descriptive activity labels
+        activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+        
+        #reads in the Y data for both the test and train Y data, and combines it into one
+        testY <- read.table("./UCI HAR Dataset/test/y_test.txt")
+        trainY <- read.table("./UCI HAR Dataset/train/y_train.txt")
+        combinedY <- rbind(testY, trainY)
+        
+        #applies the activity labels as a factor to the Y data. Binds the Y and X data into one
+        colnames(combinedY) <- c("activity")
+        combinedData <- cbind(combinedY, combinedX)
+        combinedData$activity <- factor(combinedData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+        
+        #reads in the Subjects label for both the test and train data and combines them
+        testSubjects <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+        trainSubjects <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+        combinedSubjects <- rbind(testSubjects,trainSubjects)
+        
+        #adds to the main data table the Subject IDs
+        colnames(combinedSubjects) <- c("Subject_ID")
+        combinedData <- cbind(combinedSubjects, combinedData)
+        
+        #filters out the data to only include mean() and std() measurements
+        includeCols <- grep("mean\\(\\)|std\\(\\)", colnames(combinedData))
+        includeCols <- c(1,2, includeCols)
+        combinedData <- combinedData[,includeCols]      
+        
+        #uses reshape2 package to melt and dcast the data to summarize showing the
+        #mean of each variable by Subject ID and activity.
+        #Returns this summary data as the output of the function.
+        library(reshape2)
+        meltTidy <- melt(combinedData, id=c("Subject_ID", "activity"), measure.vars = colnames(combinedData[,3:68]))
+        processedData <- dcast(meltTidy, Subject_ID + activity ~ variable, mean)
+        
+}
+
+
